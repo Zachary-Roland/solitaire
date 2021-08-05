3bd4b5c843a2ff95d4d4c1635518ccd58595c99b
Zachary-Roland <zsolo95@gmail.com>
2021-08-05 16:32:55 -0500

    implements mongoose in server

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
+const mongoose = require("mongoose");
 const express = require("express");
+const mongooseConf = require("./server/config/mongo.conf");
 const app = express();
 const port = process.env.PORT || 3200;
 const userRoutes = require("./server/routes/users.routes");
+
+mongooseConf(mongoose);
 
 app.use(express.json());
 app.use(express.static(__dirname + "/build"));
