11abed517ecc9f9a9809174b700a3792fe3c0018
Zachary-Roland <zsolo95@gmail.com>
2021-08-05 15:33:15 -0500

    troubleshooting error

--- server/config/mongo.conf.js
+++ server/config/mongo.conf.js
@@ -1,32 +1,37 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
+
+const mongoDB = process.env.MONGODB_URI;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
 
 export default (mongoose) => {
   function gracefulExit() {
-    mongoose.connection.close(() => {
+    db.close(() => {
       console.log(
         "Mongoose connection has disconnected through app termination"
       );
       process.exit(0);
     });
   }
-  mongoose.connection.on("connected", (ref) => {
+  db.on("connected", (ref) => {
     console.log(
       `Successfully connected to ${process.env.NODE_ENV} database on startup`
     );
   });
-  mongoose.connection.on("error", (err) => {
+  db.on("error", (err) => {
     console.error(
       `Failed to connect to ${process.env.NODE_ENV} database on startup`,
       err
     );
   });
-  mongoose.connection.on("disconnected", () => {
+  db.on("disconnected", () => {
     console.log(
       `Mongoose default connection to ${process.env.NODE_ENV} database disconnected`
     );
   });
   process.on("SIGINT", gracefulExit).on("SIGTERM", gracefulExit);
-  mongoose.connect(process.env.MONGO_URI, { useMongoClient: true }, (error) => {
+  mongoose.connect(mongoDB, { useMongoClient: true }, (error) => {
     if (error) throw error;
   });
 };
