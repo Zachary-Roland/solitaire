d9a8c936e27b39e2a3fbb5c01996acc5f799aa77
Zachary-Roland <zsolo95@gmail.com>
2021-08-06 15:09:19 -0500

    changes expand to sm

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
   return (
     <Router>
       <nav>
-        <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar bg="dark" variant="dark" expand="sm">
           <Navbar.Brand>Solitaire</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
