c1ebd70343d740623b08b9f1ab53124654b7188c
Zachary-Roland <zsolo95@gmail.com>
2021-08-06 10:46:40 -0500

    adds passport to index

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const express = require("express");
+const passport = require("passport");
 const mongooseConf = require("./server/config/mongo.conf");
+const configPassport = require("./server/config/passport.conf");
 const app = express();
 const port = process.env.PORT || 3200;
 const userRoutes = require("./server/routes/users.routes");
 
 mongooseConf(mongoose);
+configPassport(passport);
 
 app.use(express.json());
 app.use(express.static(__dirname + "/build"));
+app.use(passport.initialize);
 
 app.use("/api/users", userRoutes);
 
