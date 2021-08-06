ad480107dd3c498457eb7dfc10d8a9a94cdf014a
Zachary-Roland <zsolo95@gmail.com>
2021-08-06 09:59:27 -0500

    adds uuid to schema

--- server/models/users.models.js
+++ server/models/users.models.js
@@ -6,27 +6,28 @@ let userSchema = mongoose.Schema({
   password: String,
   email: { type: String, unique: true },
   highscore: { type: Number, default: 0 },
+  uuid: String,
   jwthash: { type: String },
 });
 
 // Example functions from class notes
 // TODO not currently able to pull this function in users.routes so copying functionality. Find Fix.
-userSchema.methods.generateHash = function (password) {
-  return bcrypt.hashSync(password, bcrypt.genSaltSync(), null);
-};
+// userSchema.methods.generateHash = function (password) {
+//   return bcrypt.hashSync(password, bcrypt.genSaltSync(), null);
+// };
 
-userSchema.methods.validPassword = async function (password) {
-  let compare = await bcrypt.compareSync(password, this.local.password);
-  return compare;
-};
+// userSchema.methods.validPassword = async function (password) {
+//   let compare = await bcrypt.compareSync(password, this.local.password);
+//   return compare;
+// };
 
-userSchema.methods.sanitize = function () {
-  return {
-    _id: this._id,
-    username: this.local.username,
-    email: this.local.email,
-    highscore: this.local.highscore,
-  };
-};
+// userSchema.methods.sanitize = function () {
+//   return {
+//     _id: this._id,
+//     username: this.local.username,
+//     email: this.local.email,
+//     highscore: this.local.highscore,
+//   };
+// };
 
 module.exports = mongoose.model("User", userSchema);
