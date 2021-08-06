05347a699eda9316c01b52834fb616f21ee9280d
Zachary-Roland <zsolo95@gmail.com>
2021-08-06 13:42:51 -0500

    adds Routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,59 @@
+// imports CSS
 import "./App.css";
+// imports components
 import AboutPage from "./components/AboutPage";
 import Login from "./components/Login";
-import SigninPage from "./components/SigninPage";
 import Signup from "./components/Signup";
 import SolitairePage from "./components/SolitairePage";
+// imports routing
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <AboutPage />
-      <Login />
-      <SigninPage />
-      <Signup />
-      <SolitairePage />
-    </div>
+    <Router>
+      <nav>
+        {!username && (
+          <>
+            <NavLink to="/login">Login</NavLink>
+            <NavLink to="/signup">Signup</NavLink>
+          </>
+        )}
+        {username && (
+          <>
+            <NavLink to="/game">Game</NavLink>
+            <NavLink to="/about">About</NavLink>
+            <NavLink
+              to="/login"
+              // onClick-{() => logout()}
+            >
+              Logout
+            </NavLink>
+          </>
+        )}
+      </nav>
+      <main>
+        <div className="App">
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/about">
+            <AboutPage />
+          </Route>
+          <Route path="/game">
+            <SolitairePage />
+          </Route>
+        </div>
+      </main>
+    </Router>
   );
 }
 
