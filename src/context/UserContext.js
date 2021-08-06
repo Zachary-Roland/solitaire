27e78af6351cad8e1b70785dce4f0e1e2a05c167
Zachary-Roland <zsolo95@gmail.com>
2021-08-06 13:51:42 -0500

    starts UserContext

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,2 +1,8 @@
-import React = require("react");
+import React from "react";
+import useFetch from "../hooks/useFetch";
 
+export const UserContext = createContext(null);
+
+export function UserProvider(props) {
+    const { callAPI: }
+}
