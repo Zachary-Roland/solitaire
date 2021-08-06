4a739bf98e975e753161fb65ccc029abe5ec1376
Zachary-Roland <zsolo95@gmail.com>
2021-08-06 13:59:04 -0500

    finishes UserContext

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import useFetch from "../hooks/useFetch";
 
 export const UserContext = createContext(null);
 
 export function UserProvider(props) {
-    const { callAPI: }
+  const { callAPI: logoutAPI } = useFetch("GET");
+  const { callAPI: validate } = useFetch("GET");
+  const [username, setUsername] = useState(null);
+  const login = useCallback((uName) => {
+    setUsername(uName);
+  }, []);
+  const logout = useCallback(async () => {
+    const res = await logoutAPI("/api/users/logout");
+    if (res.success) {
+      setUsername(null);
+    }
+  }, []);
+  useEffect(() => {
+    async function valid() {
+      const res = await validate("/api/users/validate");
+      if (res.success) {
+        login(res.data.username);
+      }
+    }
+    valid();
+  }, []);
+  return (
+    <UserContext.Provider value={{ username, login, logout }}>
+      {props.children}
+    </UserContext.Provider>
+  );
 }
