4a50a189a11fca8b39e390ca5de663f2c7725ed9
Zachary-Roland <zsolo95@gmail.com>
2021-08-08 17:40:38 -0500

    removes pile txt

--- src/components/Tableau.js
+++ src/components/Tableau.js
@@ -9,39 +9,36 @@ const Tableau = () => {
       <Row>
         {tableau.map((arr) => {
           return (
-            <>
-              <Col>
-                <h4>{`Pile has ${arr.length} cards`}</h4>
-                <Row>
-                  {arr.map((obj) => {
-                    if (obj.isFaceUp) {
-                      return (
-                        <Col>
-                          <div>
-                            <img
-                              className="cardImg"
-                              src={`./cardicons/${obj.suit}${obj.face}.png`}
-                              alt={`${obj.face} of ${obj.suit}`}
-                            />
-                          </div>
-                        </Col>
-                      );
-                    } else {
-                      return (
-                        <Col>
-                          <div>
-                            <img
-                              className="cardImg"
-                              src={`./cardicons/backwardscard.png`}
-                            />
-                          </div>
-                        </Col>
-                      );
-                    }
-                  })}
-                </Row>
-              </Col>
-            </>
+            <Col key={arr.length}>
+              <Row>
+                {arr.map((obj) => {
+                  if (obj.isFaceUp) {
+                    return (
+                      <Col>
+                        <div>
+                          <img
+                            className="cardImg"
+                            src={`./cardicons/${obj.suit}${obj.face}.png`}
+                            alt={`${obj.face} of ${obj.suit}`}
+                          />
+                        </div>
+                      </Col>
+                    );
+                  } else {
+                    return (
+                      <Col>
+                        <div>
+                          <img
+                            className="cardImg"
+                            src={`./cardicons/backwardscard.png`}
+                          />
+                        </div>
+                      </Col>
+                    );
+                  }
+                })}
+              </Row>
+            </Col>
           );
         })}
       </Row>
