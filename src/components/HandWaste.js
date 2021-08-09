d2e44aa646199f642ae8058b0ce100cb37bf0e80
Zachary-Roland <zsolo95@gmail.com>
2021-08-09 07:36:00 -0500

    changes click functionality

--- src/components/HandWaste.js
+++ src/components/HandWaste.js
@@ -27,52 +27,63 @@ const HandWaste = () => {
   }, [hand, waste]);
 
   return (
-    <Row>
-      {/* This is the hand pile */}
-      <Col>
-        <div className="pilePlace">
-          {hand.length > 0 && (
-            <img
-              className="cardImg"
-              src={`./cardicons/backwardscard.png`}
-              onClick={() => {
-                if (hand.length > 0) {
-                  setWaste([...waste, hand.pop()]);
-                  // console.log(waste);
-                } else {
-                  console.log("hand is empty");
-                }
-              }}
-            />
-          )}
-        </div>
-        <h6>Hand</h6>
-      </Col>
-      {/* this is the waste pile */}
-      <Col>
-        <div className="pilePlace">
-          {wasteLength > 0
-            ? waste.map((v) => {
+    <>
+      <Row>
+        {/* This is the hand pile */}
+        <Col xs={2} className="pilePlace">
+          <div>
+            {hand.length > 0 ? (
+              <img
+                className="cardImg"
+                src={`./cardicons/backwardscard.png`}
+                onClick={() => {
+                  if (hand.length > 0) {
+                    setWaste([...waste, hand.pop()]);
+                    // console.log(waste);
+                  }
+                }}
+              />
+            ) : (
+              <div
+                style={{ height: 100, width: 100 }}
+                onClick={() => {
+                  if (hand.length === 0) {
+                    setHand(waste);
+                    setWaste([]);
+                  }
+                }}
+              ></div>
+            )}
+          </div>
+          <h6>Hand</h6>
+        </Col>
+        {/* this is the waste pile */}
+        <Col xs={2} className="pilePlace">
+          <div>
+            {wasteLength > 0 ? (
+              waste.map((v) => {
                 if (v === currWasteCard) {
                   return (
                     <img
                       src={`./cardicons/${currWasteCard.suit}${currWasteCard.face}.png`}
-                      onClick={() => {
-                        if (hand.length === 0) {
-                          setHand(waste);
-                          setWaste([]);
-                        }
-                      }}
                     />
                   );
                 }
               })
-            : null}
-        </div>
-        <h6>Discard</h6>
-      </Col>
-      {/*  */}
-    </Row>
+            ) : (
+              <div style={{ height: 100, width: 100 }}></div>
+            )}
+          </div>
+          <h6>Discard</h6>
+        </Col>
+        {/*  */}
+      </Row>
+      <Row>
+        <Col>
+          <br />
+        </Col>
+      </Row>
+    </>
   );
 };
 
